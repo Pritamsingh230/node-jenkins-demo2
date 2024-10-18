e4f3d4181f19d889c825880462afabab95041dfc
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-10-18 16:31:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+app.get('/', function (req, res) {
+    res.send('{ "response": "Welcome to the Node.js App!" }');
+});
 app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": "It works!" }');
 });
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port " + (process.env.PORT || 3000));
+});
 module.exports = app;
